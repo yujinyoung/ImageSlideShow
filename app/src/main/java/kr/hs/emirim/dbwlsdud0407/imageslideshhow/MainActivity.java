8870bca480d8ae8cd46403a1e74407424dc7b68f
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-05-26 20:13:13 +0900

    #2 Event programing

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/imageslideshhow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/imageslideshhow/MainActivity.java
@@ -2,12 +2,35 @@ package kr.hs.emirim.dbwlsdud0407.imageslideshhow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ViewFlipper flip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flip=(ViewFlipper)findViewById(R.id.but_flip);
+        flip.setFlipInterval(1000);
+        Button butstart=(Button)findViewById(R.id.but_start);
+        butstart.setOnClickListener(this);
+        Button butstop=(Button)findViewById(R.id.but_stop);
+        butstop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.but_start:
+                flip.startFlipping();
+                break;
+            case R.id.but_stop:
+                flip.stopFlipping();
+                break;
+
+        }
     }
 }
